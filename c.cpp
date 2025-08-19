c62da1eef812ce65e0b62a2f556d24d9c3ee81eb
Deepakab17 <deepaknab17@gmail.com>
2025-08-19 23:16:25 +0530

    armstrong

--- c.cpp
+++ c.cpp
@@ -1,4 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int main(){}
+int main(){
+//     int number=456;
+//     int rev =0;
+//     while (number!=0)
+//     {
+//         int digits= number%10;
+//           rev= rev*10+digits;
+//          number=number/10;
+//     }
+//     cout<<rev;
+// }
+// int number=121;
+// int rev=0;
+// int newnumber=number;
+//   while (number!=0)
+//      {
+//          int digits= number%10;
+//            rev= rev*10+digits;
+//           number=number/10;
+//      }
+//         cout<<rev <<endl;
+    
+// if (newnumber==rev)
+// {
+//     cout<<"this is a pelindrome";
+// }
+// else{
+//     cout<<"this is not a pelindrome";
+// }
+int number=153;
+int original=number;
+int sum=0;
+  while (number!=0)
+     {
+         int digits= number%10;
+          number=number/10;
+          sum= sum+digits*digits*digits;
+     }
+    
+if (sum==original)
+{
+    cout<<"this is a armstrong number";
+}
+else{
+    cout<<"this is not a armstrong number";
+}
+return 0;
+}
